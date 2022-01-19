ae6d68447f94d3b0b36752b53ce868027497c100
daniel-backman <daniel.backman@ki.se>
2022-01-19 05:24:53 +0100

    add projects and updates to monster model

---
+++ CarTrauma2/monster.gd
@@ -0,0 +1,31 @@
+extends KinematicBody
+
+# define variables
+onready var nav = $"../Navigation" as Navigation
+onready var car = $"../car" as VehicleBody
+var path = []
+var current_node = 0
+var speed = 2
+
+# nessecary?
+func _ready():
+	pass
+
+# monster movement
+func _physics_process(delta):
+	if current_node < path.size():
+		var direction: Vector3 = path[current_node] - global_transform.origin
+		if direction.length() < 1:
+			current_node += 1
+		else:
+			move_and_slide(direction.normalized() * speed)
+
+# update path to car
+func update_path(target_position):
+	path = nav.get_simple_path(global_transform.origin, target_position)
+
+# timer for updating path to car
+func _on_Timer_timeout():
+	print("Looking for Car! Current node:", current_node)
+	update_path(car.global_transform.origin)
+	current_node = 0
